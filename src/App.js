3c2f20a062f385d1e409aeb922435942ea905442
Michał Gombos <michalgombos@mbp-micha.home>
2024-06-25 18:02:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/home'
+import Admin from './pages/admin'
+import NotFound from './pages/notFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/mg-admin" element={<Admin/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
